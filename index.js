f25b8aa082f519bdc45b1c0d031b6176362fe87e
Scott Saller <scottmsaller@gmail.com>
2024-08-15 11:08:02 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import inquirer from 'inquirer';
+// TODO: Create an array of questions for user input
+const questions = ["Please enter a title for your project.","Please enter a description for your project.","Please enter installation details for your project.", "Please enter information about the usage of your project.", "Please enter the contribution guidelines of your project.", "Please enter test instructions for your project."];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.
+        prompt({
+            
+        })
+}
+
+// Function call to initialize app
+init();
